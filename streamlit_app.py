24f52a85ab78a39f5f395e5f8fe5fd8d8ed6dc4f
vinith-murthy <vinith.murthy@mphasis.com>
2025-05-17 20:00:55 +0530

    Update streamlit_app.py
    updated with fruit nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
+#new section
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width =True)

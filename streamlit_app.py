15513e20d49018344f7c358efefb64af5ab85183
vinith-murthy <vinith.murthy@mphasis.com>
2025-05-17 20:39:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ cnx = st.connection("snowflake", type="snowflake")
 session = cnx.session()
 
 # Load fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width =True)
 #st.stop()
 
